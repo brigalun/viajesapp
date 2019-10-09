25a59c101c10ea663de76a8315a54abc465f97f4
Iscbr <israelscbr@gmail.com>
2019-10-09 02:06:54 -0500

    componente busqueda creado por Jesus Arturo agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { LoginComponent } from './components/login/login.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    LoginComponent
+    LoginComponent,
+    BusquedaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
